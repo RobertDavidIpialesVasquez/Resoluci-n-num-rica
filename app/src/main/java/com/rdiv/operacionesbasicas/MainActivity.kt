b14c7793cbca9ceaaa2bc117a9afce8efc76060d
RobertDavidIpialesVasquez <davidipiales12345678910@gmail.com>
2023-11-15 09:43:22 -0500

    Proyecto inicial

---
+++ app/src/main/java/com/rdiv/operacionesbasicas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rdiv.operacionesbasicas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
